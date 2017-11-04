a35b5b744f09efefcb05bc2fd74ef48ed3b74d43
Miikka Alatalo <18702596+Miikka-Alatalo@users.noreply.github.com>
2017-11-04 21:39:55 +0200

    Create setup.sh

---
+++ setup/setup.sh
@@ -0,0 +1,19 @@
+sudo timedatectl set-timezone Europe/Helsinki
+setxkbmap fi
+sudo apt-get update
+sudo apt-get install -y git tree puppet
+
+cd /home/$(whoami)/
+mkdir git
+cd git
+git clone https://github.com/Miikka-Alatalo/puppet
+
+sudo rm -rf /etc/puppet/  
+sudo ln -s /home/$(whoami)/git/puppet/puppet/ /etc/
+
+sudo puppet apply /home/$(whoami)/git/puppet/puppet/manifests/site.pp
+
+read -p "Apply site.pp? (y/n)?" choice
+case "$choice" in 
+  y|Y ) sudo puppet apply /home/$(whoami)/git/puppet/puppet/manifests/site.pp;;
+esac
