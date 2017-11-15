1a2f80b3f46a3306e5087a8e4761eccaeace2e3c
Miikka Alatalo <alatalo.miikka@gmail.com>
2017-11-15 20:55:48 +0200

    update site.pp to include .home in node names and remove extra files from hellomiikka module

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,11 +1,15 @@
-node 'virtualslave001' {
+node 'virtualslave001.home' {
 	include virtualslave001
 }
 
-node 'virtualslave002' {
+node 'virtualslave002.home' {
 	include virtualslave002
 }
 
-node 'virtualslave003' {
+node 'virtualslave003.home' {
 	include virtualslave003
+}
+
+node default {
+	include hellomiikka
 }
--- puppet/modules/hellomiikka/manifests/init.pp
+++ puppet/modules/hellomiikka/manifests/init.pp
@@ -1,18 +1,5 @@
 class hellomiikka {
-        file { '/tmp/hellomiikkafromtalo.txt':
+        file { '/tmp/hellomiikka.txt':
                 content => "TALO WAS HERE\n"
         }
-	file { '/tmp/hellomiikkafromtalosecondedition.txt':
-                content => "TALO WAS HERE ONCE AGAIN\n"
-        }
-	file { '/tmp/hellomiikkafromtalothirdedition.txt':
-                content => "TALO WAS HERE YET AGAIN\n"
-        }
-	file { '/tmp/hellomiikkafromtalofourthedition.txt':
-                content => "TALO WAS HERE YET ONCE AGAIN to test\n"
-        }
-	file { '/tmp/hellomiikkafromtalofifthhedition.txt':
-                content => "TALO WAS HERE YET ONCE AGAIN\n"
-        }
-
 }
