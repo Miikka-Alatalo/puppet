efd4ffc3a30f1fbd6b1de6499b47238b4e859652
Miikka <alatalo.miikka@gmail.com>
2017-11-22 20:29:55 +0200

    add alot

---
+++ puppet/modules/desktop/manifests/init.pp
@@ -0,0 +1,33 @@
+class desktop{
+	package { 'vlc':
+		ensure => 'installed',
+	}
+
+	file { '/etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml':
+		content => template('desktop/xfce4-keyboard-shortcuts.xml'),
+	}
+
+	file { '/usr/local/bin/superToWhiskerShortcut.sh':
+		content => template('desktop/superToWhiskerShortcut.sh'),
+	}
+
+	file { '/tmp/atom-amd64.deb':
+		ensure => present,
+		mode => 664,
+		owner => root,
+		group => root,
+		source => "puppet:///files/atom-amd64.deb"
+	}
+
+	package { 'atom':
+		provider => dpkg,
+		ensure => installed,
+		source => '/tmp/atom-amd64.deb'
+	}
+
+	file { '/home/xubuntu/.atom/config.cson':
+		owner => xubuntu,
+                group => xubuntu,
+                content => template('desktop/config.cson'),
+        }
+}
---
+++ puppet/modules/desktop/templates/superToWhiskerShortcut.sh
@@ -0,0 +1,1 @@
+xfconf-query --channel xfce4-keyboard-shortcuts --property "/commands/custom/Super_L" --create --type string --set "xfce4-popup-whiskermenu"
